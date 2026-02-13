24a3efbba54dd3080ddf9956b7e0229cc76bfba9
salkeisha <keishasetiadi48@gmail.com>
2026-02-13 13:00:22 +0700

    update percobaan 1

--- Minggu1/percobaan1_26.java
+++ Minggu1/percobaan1_26.java
@@ -1,7 +1,7 @@
-package JOBSHEET1;
+package Minggu1;
 
 import java.util.Scanner;
-public class percobaan_26 {
+public class percobaan1_26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
