67192bf6e2d628645a3b14d1f498b1a8726624b5
salkeisha <keishasetiadi48@gmail.com>
2026-02-27 13:46:26 +0700

    doneeeee

--- Minggu3/MataKuliah26.java
+++ Minggu3/MataKuliah26.java
@@ -1,10 +1,15 @@
 package Minggu3;
 
+import java.util.Scanner;
 public class MataKuliah26 {
     public String kode;
     public String nama;
     public int sks;
     public int jumlahJam;
+
+    //Mebnambahkan konstruktor default
+    public MataKuliah26() {
+    }
 
     public MataKuliah26(String kode, String nama, int sks, int jumlahJam) {
         this.kode = kode;
@@ -12,4 +17,38 @@ public class MataKuliah26 {
         this.sks = sks;
         this.jumlahJam = jumlahJam;
     }
+
+    //penambahan method tambah data
+    void tambahData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kode            : ");
+        kode = sc.nextLine();
+        System.out.print("Nama            : ");
+        nama = sc.nextLine();
+        System.out.print("Sks             : ");
+        String dummy = sc.nextLine();
+        sks = Integer.parseInt(dummy);
+        System.out.print("Jumlah Jam      : ");
+        dummy = sc.nextLine();
+        jumlahJam = Integer.parseInt(dummy);
+    }
+
+    //penambahan method cetak info
+    void cetakInfo() {
+        System.out.println("Kode        : " + kode);
+        System.out.println("Nama        : " + nama);
+        System.out.println("Sks         : " + sks);
+        System.out.println("Jumlah Jam  : " + jumlahJam);
+    }
+
+    // Untuk menjawab pertanyaan pada jobsheet
+    // public MataKuliah26(String kode, String nama) {
+    //     this.kode = kode;
+    //     this.nama = nama;
+    // }
+
+    // public MataKuliah26(int sks, int jumlahJam) {
+    //     this.sks = sks;
+    //     this.jumlahJam = jumlahJam;
+    // }
 }
--- Minggu3/MataKuliahDemo26.java
+++ Minggu3/MataKuliahDemo26.java
@@ -5,33 +5,37 @@ public class MataKuliahDemo26 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        MataKuliah26[] arrayOfMataKuliah26 = new MataKuliah26[3];
+        // Modifikasi agar panjang array menjadi input dinamis
+        System.out.print("Masukkan jumlah Matakuliah yang ingin diinput : ");
+        int jumlahMatkul = sc.nextInt();
+
+        MataKuliah26[] arrayOfMataKuliah26 = new MataKuliah26[jumlahMatkul];
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Masukkan data Matakuliah ke-" + (i+1));
-            System.out.print("Kode            : ");
-            kode = sc.nextLine();
-            System.out.print("Nama            : ");
-            nama = sc.nextLine();
-            System.out.print("Sks             : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam      : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
+            // Modifikasi percobaan 3, penambahan method kedalam file Main
+            arrayOfMataKuliah26[i] = new MataKuliah26();
+            arrayOfMataKuliah26[i].tambahData();
+            // System.out.print("Kode            : ");
+            // kode = sc.nextLine();
+            // System.out.print("Nama            : ");
+            // nama = sc.nextLine();
+            // System.out.print("Sks             : ");
+            // dummy = sc.nextLine();
+            // sks = Integer.parseInt(dummy);
+            // System.out.print("Jumlah Jam      : ");
+            // dummy = sc.nextLine();
+            // jumlahJam = Integer.parseInt(dummy);
             System.out.println("------------------------------------");
 
-            arrayOfMataKuliah26[i] = new MataKuliah26(kode, nama, sks, jumlahJam);
+            // arrayOfMataKuliah26[i] = new MataKuliah26(kode, nama, sks, jumlahJam);
         }
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < jumlahMatkul; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
-            System.out.println("Kode        : " + arrayOfMataKuliah26[i].kode);
-            System.out.println("Nama        : " + arrayOfMataKuliah26[i].nama);
-            System.out.println("Sks         : " + arrayOfMataKuliah26[i].sks);
-            System.out.println("Jumlah Jam  : " + arrayOfMataKuliah26[i].jumlahJam);
+            arrayOfMataKuliah26[i].cetakInfo();
             System.out.println("------------------------------------");
         }
         sc.close();
