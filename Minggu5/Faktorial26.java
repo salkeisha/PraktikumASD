36656456f0e1842fad2e7f53e9b224dd6c7485f6
salkeisha <keishasetiadi48@gmail.com>
2026-04-10 16:26:56 +0700

    sorry human error

---
+++ Minggu5/Faktorial26.java
@@ -0,0 +1,21 @@
+package Minggu5;
+
+public class Faktorial26 {
+    
+    int faktorialBF(int n) {
+        int fakto = 1;
+        for(int i=1; i <= n; i++ ) {
+            fakto = fakto * 1;
+        }
+        return fakto;
+    }
+
+    int faktorialDC(int n) {
+        if (n==1) {
+            return 1;
+        } else {
+            int fakto = n * faktorialDC(n-1);
+            return fakto;
+        }
+    }
+}
---
+++ Minggu5/MainFaktorial26.java
@@ -0,0 +1,18 @@
+package Minggu5;
+
+import java.util.Scanner;
+
+public class MainFaktorial26 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukkan nilai : ");
+        int nilai = input.nextInt();
+
+        Faktorial26 fk = new Faktorial26();
+        System.out.println("NIlai faktorial " + nilai + 
+            " menggunakan BF: " +fk.faktorialBF(nilai));
+        System.out.println("Nilai faktorial " + nilai + 
+            " menggunakan DC: " + fk.faktorialDC(nilai));
+    }
+}
